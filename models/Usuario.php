7fdede438a4e3a1fa4ef2075696b0e374182b8c3
Jose Luis De Miguel Pérez <jl.demiguel@alumnos.upm.es>
2022-07-30 20:08:39 +0200

    Usuarios autoconfirmados

--- models/Usuario.php
+++ models/Usuario.php
@@ -26,7 +26,7 @@ class Usuario extends ActiveRecord
         $this->password = $args['password'] ?? '';
         $this->telefono = $args['telefono'] ?? '';
         $this->admin = $args['admin'] ?? '0';
-        $this->confirmado = $args['confirmado'] ?? '0';
+        $this->confirmado = $args['confirmado'] ?? '1';
         $this->token = $args['token'] ?? '';
     }
 
